317d8f8f1b19f540e635c6653298721bbde8221d
Dixit Thiya <114086366+dixitthiya@users.noreply.github.com>
2022-11-11 12:36:34 -0600

    Create 185. Department Top Three Salaries.sql

---
+++ sql/185. Department Top Three Salaries.sql
@@ -0,0 +1,17 @@
+-- https://leetcode.com/problems/department-top-three-salaries/submissions/
+
+WITH cte AS
+  (SELECT id,
+          name,
+          salary,
+          departmentid,
+          DENSE_RANK() over(PARTITION BY departmentid
+                            ORDER BY salary DESC) AS rnk
+   FROM employee)
+
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
+FROM cte e
+INNER JOIN department d ON e.departmentid = d.id
+WHERE rnk <= 3
