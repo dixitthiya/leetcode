77223cf734be20d4ae4eb492d593ad3a736d82fa
Dixit Thiya <114086366+dixitthiya@users.noreply.github.com>
2023-03-21 19:45:13 -0500

    Update 183. Customers Who Never Order.sql

--- sql/183. Customers Who Never Order.sql
+++ sql/183. Customers Who Never Order.sql
@@ -1,7 +1,77 @@
-# https://leetcode.com/problems/customers-who-never-order/?envType=study-plan&id=sql-i
+-- https://leetcode.com/problems/customers-who-never-order/?envType=study-plan&id=sql-i
+
+-- Table: Customers
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- +-------------+---------+
+-- id is the primary key column for this table.
+-- Each row of this table indicates the ID and name of a customer.
+ 
+
+-- Table: Orders
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | customerId  | int  |
+-- +-------------+------+
+-- id is the primary key column for this table.
+-- customerId is a foreign key of the ID from the Customers table.
+-- Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+-- Write an SQL query to report all customers who never order anything.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Customers table:
+-- +----+-------+
+-- | id | name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+-- Orders table:
+-- +----+------------+
+-- | id | customerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+-- Output: 
+-- +-----------+
+-- | Customers |
+-- +-----------+
+-- | Henry     |
+-- | Max       |
+-- +-----------+
+
+-- 1. Using Subquery
 
 SELECT name AS Customers
 FROM Customers
 WHERE id not in
     (SELECT customerid
      FROM orders)
+
+-- 2. Using Left Join - Left join is used to find missing rows in other table
+
+SELECT NAME AS customers
+FROM   customers c
+       LEFT JOIN orders o
+              ON c.id = o.customerid
+WHERE  o.id IS NULL 
